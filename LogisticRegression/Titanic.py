0edc3e500813e1a2cc0d9063e4d675057f0efc49
rohan <ro.anilpnr@gmail.com>
2022-01-30 12:03:36 +0530

    Added missing age info

--- LogisticRegression/Titanic.py
+++ LogisticRegression/Titanic.py
@@ -7,6 +7,8 @@ import seaborn as sns
 train = pd.read_csv('titanic_train.csv')
 test = pd.read_csv('titanic_test.csv')
 
+# analyzing data
+
 # sns.heatmap(train.isnull(), yticklabels=False, cbar=False, cmap='viridis')
 
 sns.set_style('whitegrid')
@@ -14,7 +16,30 @@ sns.set_style('whitegrid')
 
 # sns.distplot(train['Age'].dropna(), kde=False, bins=30)
 
-sns.countplot(x='SibSp', data=train)
+# sns.countplot(x='SibSp', data=train)
+
+
+# cleaning Data
+# sns.boxplot(x='Pclass', y='Age', data=train)
+
+def impute_age(cols):
+    Age = cols[0]
+    PClass = cols[1]
+
+    if pd.isnull(Age):
+        if PClass==1:
+            return 37
+        elif PClass==2:
+            return 29
+        else:
+            return 24
+    else:
+        return Age
+
+train['Age'] = train[['Age', 'Pclass']].apply(impute_age, axis=1)
+
+# sns.heatmap(train.isnull(), yticklabels=False, cbar=False, cmap='viridis')
+
 
 plt.show()
 
