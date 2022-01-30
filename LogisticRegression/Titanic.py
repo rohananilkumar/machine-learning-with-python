3f6e988b6fe270349a289b06e1f2182a3c22dce1
rohan <ro.anilpnr@gmail.com>
2022-01-30 10:29:11 +0530

    More data analysis

--- LogisticRegression/Titanic.py
+++ LogisticRegression/Titanic.py
@@ -10,7 +10,11 @@ test = pd.read_csv('titanic_test.csv')
 # sns.heatmap(train.isnull(), yticklabels=False, cbar=False, cmap='viridis')
 
 sns.set_style('whitegrid')
-sns.countplot(x='Survived', hue='Pclass', data=train, palette='RdBu_r')
+# sns.countplot(x='Survived', hue='Pclass', data=train, palette='RdBu_r')
+
+# sns.distplot(train['Age'].dropna(), kde=False, bins=30)
+
+sns.countplot(x='SibSp', data=train)
 
 plt.show()
 
