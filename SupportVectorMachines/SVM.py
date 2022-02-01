9cfa79c16f642fbb653e6dac0113806e3220966a
rohan <ro.anilpnr@gmail.com>
2022-02-01 16:47:42 +0530

    SVC complete

--- SupportVectorMachines/SVM.py
+++ SupportVectorMachines/SVM.py
@@ -5,9 +5,25 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from sklearn.datasets import load_breast_cancer
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
 
 cancer = load_breast_cancer()
 
 cancer.keys()
 df_feat = pd.DataFrame(cancer['data'], columns=cancer['feature_names'])
 
+X=df_feat
+y=cancer['target']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
+
+model = SVC()
+
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+
+print(confusion_matrix(y_test, predictions))
+print(classification_report(y_test, predictions))
